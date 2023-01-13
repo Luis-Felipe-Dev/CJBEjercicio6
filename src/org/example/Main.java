b2bea6fd8fb3d7241cc5b3f9659014db51fbf910
Luis-Felipe-Dev <luis.fhb.2016@gmail.com>
2023-01-12 22:18:33 -0500

    Ejercicio 6

---
+++ src/org/example/Main.java
@@ -0,0 +1,130 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        String textoInvertido = reverse("Hola mundo");
+        System.out.println(textoInvertido);
+
+        System.out.println("#############################");
+
+        String[] intArray = {"Luis", "Felipe", "Huaranca", "Berrospi"};
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+        System.out.println("#############################");
+
+        int intArrayBidi[][] = new int[3][3];
+        intArrayBidi[0][0] = 1;
+        intArrayBidi[0][1] = 2;
+        intArrayBidi[0][2] = 3;
+        intArrayBidi[1][0] = 4;
+        intArrayBidi[1][1] = 5;
+        intArrayBidi[1][2] = 6;
+        intArrayBidi[2][0] = 7;
+        intArrayBidi[2][1] = 8;
+        intArrayBidi[2][2] = 9;
+
+        for (int i = 0; i < intArrayBidi.length; i++) {
+            for (int j = 0; j < intArrayBidi.length; j++) {
+                System.out.println("Posición: [" + i + "][" + j + "], Valor: " + intArrayBidi[i][j]);
+            }
+        }
+
+        System.out.println("#############################");
+
+        Vector<Integer> vector = new Vector();
+        vector.add(1);
+        vector.add(2);
+        vector.add(3);
+        vector.add(4);
+        vector.add(5);
+        System.out.println(vector);
+
+        vector.remove(1);
+        vector.remove(2);
+        System.out.println(vector);
+
+        System.out.println("#############################");
+
+        System.out.println("El uso de vectores consume mayor recursos al crecer el vector.");
+
+        System.out.println("#############################");
+
+        ArrayList<String> lista = new ArrayList<>();
+        lista.add("Luis");
+        lista.add("Felipe");
+        lista.add("Huaranca");
+        lista.add("Berrospi");
+
+        for (String item : lista) {
+            System.out.println(item);
+        }
+
+        LinkedList<String> listaEnlazada = new LinkedList<String>(lista);
+
+        for (String item : listaEnlazada) {
+            System.out.println(item);
+        }
+        System.out.println("#############################");
+
+        ArrayList<Integer> listaEnteros = new ArrayList<Integer>();
+
+        for (int i = 1; listaEnteros.size() < 10; i++) {
+            listaEnteros.add(i);
+        }
+
+        System.out.println(listaEnteros);
+
+        for (int i = 0; i < listaEnteros.size(); i++) {
+            if (listaEnteros.get(i) % 2 == 0) {
+                listaEnteros.remove(i);
+            }
+        }
+
+        System.out.println(listaEnteros);
+
+        System.out.println("#############################");
+
+        try {
+            DividePorCero(5, 0);
+            System.out.println("Demo de código");
+        } catch (ArithmeticException e) {
+            System.out.println("Esto no puede hacerse");
+        }
+
+        System.out.println("#############################");
+    }
+
+    static double DividePorCero(int n1, int n2) {
+        double result = n1 / n2;
+        return result;
+    }
+
+    public static String reverse(String texto) {
+        String textoInvertido = "";
+        char letra;
+        System.out.println(texto);
+
+        for (int i = 0; i < texto.length(); i++) {
+            letra = texto.charAt(i);
+            textoInvertido = letra + textoInvertido;
+        }
+        return textoInvertido;
+    }
+
+}
+
+//Utilizando InputStream y PrintStream, crea una función que
+// reciba dos parámetros: "fileIn" y "fileOut". La tarea de la
+// función será realizar la copia del fichero dado en el parámetro
+// "fileIn" al fichero dado en "fileOut".
+
+//Sorpréndenos creando un programa de tu elección que utilice
+// InputStream, PrintStream, excepciones, un HashMap y un ArrayList,
+// LinkedList o array.
